56a5b6c457981874427ebb7c74cf99c9a218f34d
Ethan9090 <71732564+Ethan9090@users.noreply.github.com>
2021-04-19 18:07:49 -0400

    Add files via upload

---
+++ buss.js
@@ -0,0 +1,16 @@
+coco = "";
+
+buss = "";
+
+function preload(){
+    buss = loadImage("mychair.jfif");
+}
+
+function setup(){
+
+    canvas = createCanvas(500,500);
+    canvas.center()
+
+    coco = ml5.objectDetector("cocossd",modelLoaded);
+
+}
